80b20366c4d889f01659ccb97b2710ad9df8d125
Jasper Blues <jasper.reloaded@gmail.com>
2011-09-30 17:37:08 +0800

    Add test cases.

--- Tests/Unit/SessionContextHolderSpec.m
+++ Tests/Unit/SessionContextHolderSpec.m
@@ -4,11 +4,22 @@
 
 SPEC_BEGIN(SessionContextHolderSpec)
 
+#define kXmlResponseWithSessionToken @"<CreateSessionXResponse xmlns=\"http://www.expanz.com/ESAService\">\
+<CreateSessionXResult>net.tcp://127.0.0.1:8198/SessionManager1#634530077119838630:30</CreateSessionXResult>\
+<errorMessage>Existing session abandoned [19:19]&#xD;</errorMessage></CreateSessionXResponse>"
+
 #define kXmlResponseWithError @"<CreateSessionXResponse xmlns=\"http://www.expanz.com/ESAService\">\
 <CreateSessionXResult/><errorMessage>Unknown User</errorMessage></CreateSessionXResponse>"
 
 
 describe(@"Instantiate a SessionContextHolder with the response received from the server", ^{
+    
+    it(@"should set a warning flag if session token exists, but XML errorMessage element is present.", ^{
+        SessionContextHolder* sessionContextHolder = [SessionContextHolder fromXml:kXmlResponseWithSessionToken];
+        assertThat(sessionContextHolder.sessionToken, is(notNilValue()));
+        assertThatBool(sessionContextHolder.hasWarning, is(equalToBool(YES)));
+        assertThat(sessionContextHolder.warningMessage, is(equalTo(@"Existing session abandoned [19:19]&#xD;")));        
+    });
     
     it(@"should set an error flag session token is null and XML errorMessage element is present.", ^{
         SessionContextHolder* sessionContextHolder = [SessionContextHolder fromXml:kXmlResponseWithError];               
--- expanz-iOS-SDK/expanz_ui_LoginViewController.m
+++ expanz-iOS-SDK/expanz_ui_LoginViewController.m
@@ -69,6 +69,7 @@
 }
 
 -(BOOL) textFieldShouldReturn:(UITextField*) textField {
+    [self userDidRequestLogin:nil];
     [textField resignFirstResponder]; 
     return YES;
 }
