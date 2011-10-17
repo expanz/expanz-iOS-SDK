2e5fb60e0ae6f33e97a04b865c7288172cd73baf
Jasper Blues <jasper.reloaded@gmail.com>
2011-10-17 14:58:54 +0800

    build using Xcode4.2 and llvm compiler.

--- Tests/TestRunner/main.m
+++ Tests/TestRunner/main.m
@@ -14,17 +14,6 @@
 int main(int argc, char *argv[]) {
     NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
     
-    const char *prefix = "GCOV_PREFIX";
-    const char *prefixValue = [[NSHomeDirectory() stringByAppendingPathComponent:@"Documents"]
-                               cStringUsingEncoding:NSASCIIStringEncoding];
-    const char *prefixStrip = "GCOV_PREFIX_STRIP";
-    const char *prefixStripValue = "1";
-    // This sets an environment variable which tells gcov where to put the .gcda files.
-    setenv(prefix, prefixValue, 1);
-    // This tells gcov to strip the default prefix, and use the filepath that we just declared.
-    setenv(prefixStrip, prefixStripValue, 1);
-    
-    
     //This lets the simulator get itself in order.
     [NSThread sleepForTimeInterval:0.5];
     int retVal = UIApplicationMain(argc, argv, nil, @"CedarApplicationDelegate");
