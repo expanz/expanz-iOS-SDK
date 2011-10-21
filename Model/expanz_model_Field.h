c8539a6b65f35ee816c0e86160fd17606d0f97c3
Jasper Blues <jasper.reloaded@gmail.com>
2011-10-21 16:32:10 +0800

    Update docs.

--- Model/expanz_model_Field.h
+++ Model/expanz_model_Field.h
@@ -14,14 +14,31 @@
 
 @interface expanz_model_Field : NSObject
 
+
+/* ================================================================================================================== */
+/** Unique identifier. */
 @property (nonatomic, readonly) NSString* fieldId; 
+
+/** Indicates if the field is mandatory. */
 @property (nonatomic, readonly, getter = isNullable) BOOL nullable; 
+
+/** The value the field is initialized with, ie user is presented initially with this value. */
 @property (nonatomic, readonly) NSString* defaultValue; 
+
+/** The field's datatype. TODO: This should not be represented by a string value. */
 @property (nonatomic, readonly) NSString* datatype;
+
+/** Indicates the client model is not yet synchronized with the server model. */
 @property (nonatomic, readonly, getter = isDirty) BOOL dirty; 
 
+/** Reference to the activity this field belongs to. */
 @property (nonatomic, readwrite, assign) expanz_model_ActivityInstance* parentActivity;
+
+/** The value held by this field. */
 @property (nonatomic, readwrite, retain) NSString* value;
+
+
+/* ================================================================================================================== */
 
 - (id) initWithFieldId:(NSString*)fieldId nullable:(BOOL)nullable defaultValue:(NSString*)defaultValue 
               dataType:(NSString*)datatype; 
