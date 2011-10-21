6763d6391d15335b456ce9e38ded3f0ccad83802
Jasper Blues <jasper.reloaded@gmail.com>
2011-10-21 15:14:45 +0800

    oops - can't nest cedar it blocks.

--- Tests/Unit/Model/FieldSpec.m
+++ Tests/Unit/Model/FieldSpec.m
@@ -14,13 +14,13 @@
 
 SPEC_BEGIN(FieldSpec)
 
+__block Field* field; 
+
+beforeEach(^{
+    field = [[Field alloc] initWithFieldId:@"op1" nullable:NO defaultValue:nil dataType:@"number"]; 
+});
+
 describe(@"Object creation.", ^{
-    
-    __block Field* field; 
-    
-    beforeEach(^{
-        field = [[Field alloc] initWithFieldId:@"op1" nullable:NO defaultValue:nil dataType:@"number"]; 
-    });
     
     it(@"should allow initialization with fieldId, nullable, defaultValue and datatype attributes.", ^{
         assertThat([field fieldId], equalTo(@"op1")); 
@@ -28,55 +28,41 @@ describe(@"Object creation.", ^{
         assertThat([field defaultValue], nilValue()); 
         assertThat([field datatype], equalTo(@"number"));
     });
-    
-    afterEach(^{
-        [field release];
-    });
-    
+        
 });
 
 describe(@"Editing", ^{
     
-    __block Field* field; 
-    
-    beforeEach(^{
-        field = [[Field alloc] initWithFieldId:@"op1" nullable:NO defaultValue:nil dataType:@"number"]; 
-    });
-    
-    
     it(@"should include a method to set a new value, as a result of user interaction.", ^{
-        [field userDidEditWithValue:@"new value"];
-        
-        assertThat(field.value, equalTo(@"new value"));
-        
-        it(@"should set a dirty flag to indicate the user changed a value, but the value has not yet been recieved \
-           by the server",  ^{
-               
-               assertThatBool(field.isDirty, equalToBool(YES));
-               
-        });
-        
-        
+        [field userDidEditWithValue:@"new value"];        
+        assertThat(field.value, equalTo(@"new value"));                        
     });
     
-    
+    it(@"should set a dirty flag to indicate the user changed a value, but the value has not yet been recieved \
+       by the server",  ^{
+           [field userDidEditWithValue:@"new value"];        
+           assertThatBool(field.isDirty, equalToBool(YES));
+    });
+
+});
+
+describe(@"synchronizing state with the server/cloud.", ^{
+
     it(@"should include a method to set a new value, as a result of server processing a delta request.", ^{
         [field completedStateChangeWithValidation:@"new validated value"]; 
         assertThat(field.value, equalTo(@"new validated value"));
-                
-        it(@"should revert back to a clean state, after the server has registered the delta. ", ^{
-            assertThatBool(field.isDirty, equalToBool(NO));
-            
-        });
-
         
     });
     
-    
-    afterEach(^{
-        [field release];
+    it(@"should revert back to a clean state, after the server has registered the delta. ", ^{
+        [field completedStateChangeWithValidation:@"new validated value"]; 
+        assertThatBool(field.isDirty, equalToBool(NO));        
     });
 
+});
+
+afterEach(^{
+    [field release];
 });
 
 
