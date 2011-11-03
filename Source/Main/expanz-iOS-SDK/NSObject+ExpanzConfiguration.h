535ca034ef148dffcf06a6bd40816eac64dad566
Jasper Blues <jasper.reloaded@gmail.com>
2011-11-03 14:24:37 +0800

    add more missing files.

---
+++ Source/Main/expanz-iOS-SDK/NSObject+ExpanzConfiguration.h
@@ -0,0 +1,19 @@
+////////////////////////////////////////////////////////////////////////////////
+//
+//  EXPANZ
+//  Copyright 2008-2011 EXPANZ
+//  All Rights Reserved.
+//
+//  NOTICE: Expanz permits you to use, modify, and distribute this file
+//  in accordance with the terms of the license agreement accompanying it.
+//
+////////////////////////////////////////////////////////////////////////////////
+
+
+#import <Foundation/Foundation.h>
+
+@interface NSObject (ExpanzConfiguration)
+
+- (void) setExpanzConfiguration:(NSString*)configurationFileName;
+
+@end
