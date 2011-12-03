fe2662063c28e45090f97f28a0d409618b6d6923
Jasper Blues <jasper.reloaded@gmail.com>
2011-12-03 20:09:06 +0800

    Add test coverage.

---
+++ Source/Specs/Unit/UserInterface/AlertBoxSystemEventReporterSpec.m
@@ -0,0 +1,44 @@
+////////////////////////////////////////////////////////////////////////////////
+//
+//  EXPANZ
+//  Copyright 2008-2011 EXPANZ
+//  All Rights Reserved.
+//
+//  NOTICE: Expanz permits you to use, modify, and distribute this file
+//  in accordance with the terms of the license agreement accompanying it.
+//
+////////////////////////////////////////////////////////////////////////////////
+
+#import "SpecHelper.h"
+#import "expanz_ui_SystemEventReporter.h"
+#import "expanz_ui_AlertBoxSystemEventReporter.h"
+
+SPEC_BEGIN(AlertBoxSystemEventReporterSpec)
+
+    __block id<expanz_ui_SystemEventReporter> reporter; 
+
+    beforeEach(^{
+        reporter = [[AlertBoxSystemEventReporter alloc] init];
+    });       
+
+    it(@"should allow assigning a UIAlertViewDelegate", ^{
+        
+        id<UIAlertViewDelegate> delegate = nil; 
+        AlertBoxSystemEventReporter* alertBoxSystemEventReporter = (AlertBoxSystemEventReporter*) reporter;
+        alertBoxSystemEventReporter.delegate = delegate;
+        assertThat(alertBoxSystemEventReporter.delegate, nilValue());
+    });
+    
+    it(@"should allow reporting error messages", ^{
+        [reporter reportErrorWithReason:@"Something went wrong."];               
+    });
+    
+    it(@"should allow reporting a warning", ^{
+        [reporter reportWarningWithReason:@"Sky is blue."];
+    });
+
+    it(@"should allow reporting arbitrary messages", ^{
+        [reporter reportMessageWithTitle:@"Hey" message:@"This is a message."];
+    });
+
+SPEC_END
