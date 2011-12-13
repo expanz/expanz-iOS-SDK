20331f36afaa5ac96e79e3a0ee2974a2b8c41bbb
Jasper Blues <jasper.reloaded@gmail.com>
2011-12-13 18:16:08 +0800

    Add Project Templates.

---
+++ Source/Project Templates/Expanz Iphone.xctemplate/main.m
@@ -0,0 +1,20 @@
+////////////////////////////////////////////////////////////////////////////////
+//
+//  EXPANZ
+//  Copyright 2008-2011 EXPANZ
+//  All Rights Reserved.
+//
+//  NOTICE: Expanz permits you to use, modify, and distribute this file
+//  in accordance with the terms of the license agreement accompanying it.
+//
+////////////////////////////////////////////////////////////////////////////////
+
+#import <UIKit/UIKit.h>
+#import "expanz_iOS_SDKConfiguration.h"
+
+int main(int argc, char *argv[]) {
+    NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, nil);
+    [pool release];
+    return retVal;
+}
