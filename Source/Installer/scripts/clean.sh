55a8b8faf400b1f86501e67136f9c53647e6cccb
Jasper Blues <jasper.reloaded@gmail.com>
2011-12-14 13:23:56 +0800

    add the installer source.

---
+++ Source/Installer/scripts/clean.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+rm -fr /Developer/Library/Frameworks/Expanz.framework
+rm -fr /Developer/Library/Xcode/Templates/Expanz
+rm -fr  ~/Library/Developer/Xcode/Templates/Expanz
---
+++ Source/Installer/scripts/links.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+cd /Developer/Library/Frameworks
+rm -fr ExpanzIOS.framework
+sudo ln -s /Developer/Expanz/Frameworks/ExpanzIOS.framework ExpanzIOS.framework
+
