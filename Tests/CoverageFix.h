ae4b18dd6c4e30174f040e18736dad5bd9c27b4d
Jasper Blues <jasper.reloaded@gmail.com>
2012-02-21 10:49:13 +0800

    Fix code coverage with Kiwi.

---
+++ Tests/CoverageFix.h
@@ -0,0 +1,23 @@
+////////////////////////////////////////////////////////////////////////////////
+//
+//  EXPANZ
+//  Copyright 2008-2011 EXPANZ
+//  All Rights Reserved.
+//
+//  NOTICE: Expanz permits you to use, modify, and distribute this file
+//  in accordance with the terms of the license agreement accompanying it.
+//
+////////////////////////////////////////////////////////////////////////////////
+#import <Foundation/Foundation.h>
+
+/**
+* This fixes a problem related to an Apple implementation of a core unix api.
+*/
+@interface CoverageFix : NSObject
+
+
+FILE* fopen$UNIX2003(const char* filename, const char* mode);
+
+size_t fwrite$UNIX2003(const void* ptr, size_t size, size_t nitems, FILE* stream);
+
+@end
---
+++ Tests/CoverageFix.m
@@ -0,0 +1,24 @@
+////////////////////////////////////////////////////////////////////////////////
+//
+//  EXPANZ
+//  Copyright 2008-2011 EXPANZ
+//  All Rights Reserved.
+//
+//  NOTICE: Expanz permits you to use, modify, and distribute this file
+//  in accordance with the terms of the license agreement accompanying it.
+//
+////////////////////////////////////////////////////////////////////////////////
+#import "CoverageFix.h"
+
+
+@implementation CoverageFix
+
+FILE* fopen$UNIX2003(const char* filename, const char* mode) {
+    return fopen(filename, mode);
+}
+
+size_t fwrite$UNIX2003(const void* ptr, size_t size, size_t nitems, FILE* stream) {
+    return fwrite(ptr, size, nitems, stream);
+}
+
+@end
