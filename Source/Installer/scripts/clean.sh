e878d91282c517b077debb1ba8601d1d8d2390c0
Jasper Blues <jasper.reloaded@gmail.com>
2012-02-29 12:28:56 +0800

    Update installer.

--- Source/Installer/scripts/clean.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-rm -fr /Developer/Library/Frameworks/expanzIOS.framework
-rm -fr /Developer/Library/Xcode/Templates/expanz
-rm -fr  ~/Library/Developer/Xcode/Templates/expanz
--- Source/Installer/scripts/links.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-cd /Developer/Library/Frameworks
-rm -fr expanziOS.framework
-sudo ln -s /Developer/expanz/Frameworks/expanziOS.framework expanziOS.framework
-
